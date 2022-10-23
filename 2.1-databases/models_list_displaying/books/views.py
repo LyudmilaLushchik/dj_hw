f6c31d3fe46c50a21c25fa24dc00c5aaeb916954
LyudmilaLushchik <lyudmila_c@rambler.ru>
2022-10-23 21:43:48 +0300

    Changed books_view_by_date

--- 2.1-databases/models_list_displaying/books/views.py
+++ 2.1-databases/models_list_displaying/books/views.py
@@ -1,3 +1,5 @@
+from django.core.paginator import Paginator
+
 from django.shortcuts import render
 from books.models import Book
 
@@ -10,20 +12,42 @@ def books_view(request):
 
 def books_view_by_date(request, p_date):
     template = 'books/books_list_by_date.html'
-    books = Book.objects.all()
-    b_by_date = books.filter(pub_date=p_date)
-    b_list = list(books)
+    books = Book.objects.all().order_by('pub_date')
+    dates_list = Book.objects.values('pub_date').distinct('pub_date').order_by('pub_date')    
+    paginator = Paginator(dates_list, 1)
+    filter_date = p_date
+    has_found = False
+    for p in paginator.page_range:        
+        pg = paginator.get_page(p)
+        if str(pg.object_list.values()[0].get('pub_date')) == p_date:
+            page_number = pg.number
+            has_found = True
+            break
+    if not has_found: 
+        page_number = 1
+        page_obj = paginator.get_page(page_number)
+        filter_date =page_obj.object_list.values()[0].get('pub_date')
 
-    if b_list.index(b_by_date[0]) != 0:
-        previous_date = Book.objects.get(id=b_list[b_list.index(b_by_date[0]) - 1].id).pub_date
+    b_by_date = books.filter(pub_date=filter_date)
+    page_obj = paginator.get_page(page_number)
+
+    if page_obj.has_previous():
+        prev_page = paginator.get_page(page_obj.previous_page_number())
+        prev_date = prev_page.object_list.values()[0].get('pub_date')
     else:
-        previous_date = None
+        prev_date = None
 
-    if b_list.index(b_by_date[0]) < len(books) - 1:
-        next_date = Book.objects.get(id=b_list[b_list.index(b_by_date[0]) + 1].id).pub_date
+    if page_obj.has_next():
+        next_page = paginator.get_page(page_obj.next_page_number())
+        next_date = next_page.object_list.values()[0].get('pub_date')
     else:
         next_date = None
-
-    context = {'books': b_by_date, 'previous_date': previous_date, 'next_date': next_date}
+    
+    context = {
+        'books': b_by_date, 
+        'page': page_obj, 
+        'prev_date': prev_date,
+        'next_date': next_date,   
+    }
 
     return render(request, template, context)
